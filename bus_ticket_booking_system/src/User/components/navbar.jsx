5d3cdf042eefb0084e79e9b49e8fd776721507ca
Konkala Sreenivasareddy <sreenivasareddy2212@gmail.com>
2025-07-23 19:01:45 +0530

    added new page links to navbar

--- bus_ticket_booking_system/src/User/components/navbar.jsx
+++ bus_ticket_booking_system/src/User/components/navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { use } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
+  const handleLogout = () =>{
+    alert("Logout successful!");
+    navigate("/")
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
       <Link className="navbar-brand fw-bold" to="/">
@@ -18,25 +23,38 @@ function Navbar() {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link className="nav-link" to="/">
+            <Link className="nav-link text-white" to="/user">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/bookings">
+            <Link className="nav-link text-white" to="/user/viewticket">
+              View Ticket
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/user/cancelticket">
+              Cancel Ticket
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/user/mybookings">
               My Bookings
             </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/profile">
+            <Link className="nav-link text-white" to="/user/profile">
               Profile
             </Link>
           </li>
           <li className="nav-item">
-            <button className="btn btn-danger ms-2">Logout</button>
+            <button className="btn btn-danger ms-2" onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </div>
